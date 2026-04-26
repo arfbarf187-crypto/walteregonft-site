7308b008da141077f0763af66223b3bb9c4a1ea0
arfbarf187-crypto <arfbarf187@gmail.com>
2026-04-26 00:28:33 -0500

    Create nft.js

---
+++ nft.js
@@ -0,0 +1,9 @@
+const nft = {
+  name: "Leo da Porch Cat",
+  collection: "Hood Catz",
+  rarity: "Legendary",
+  location: "Chicago",
+  chain: "HC Platinum"
+};
+
+console.log(JSON.stringify(nft, null, 2));
